8998344875a148537c7913e00c1fd48857d63e6c
sasidn <sasidharannissy@gmail.com>
2023-09-04 22:50:16 +0200

    working

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 import os
 from flask import Flask, render_template, request, redirect
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
-
+#from sqlalchemy import create_engine
+import MySQLdb
 # Load environment variables from the .env file
 load_dotenv()
 
 # Get the database connection URL from the environment variables
-db_url = os.getenv("DATABASE_URL")
+#db_url = os.getenv("DATABASE_URL")
 
 # Create the SQLAlchemy engine
-engine = create_engine(db_url,connect_args={"ssl": {"ssl_ca": "/etc/ssl/cert.pem"}
-})
+connection = MySQLdb.connect(
+  host= os.getenv("DB_HOST"),
+  user=os.getenv("DB_USERNAME"),
+  passwd= os.getenv("DB_PASSWORD"),
+  db= os.getenv("DB_NAME"),
+  autocommit = True,
+  ssl_mode = "VERIFY_IDENTITY",
+  ssl      = {
+    "ca": "/etc/ssl/cert.pem"
+  }
+)
+#engine = create_engine(db_url,connect_args={"ssl": {"ssl_ca": "/etc/ssl/cert.pem"}})
 app = Flask(__name__)
 
 @app.route('/')
